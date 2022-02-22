8723e195a51971110cace17f13a88b4b7e0d7a1c
injune2 <os2864@naver.com>
2022-02-22 16:02:24 +0900

    - clientVo 생성 - addClient Form 추가

---
+++ oup/src/main/java/com/kh/oup/client/vo/ClientVo.java
@@ -0,0 +1,21 @@
+package com.kh.oup.client.vo;
+
+import lombok.Data;
+
+@Data
+public class ClientVo {
+	int cNo;
+	String cName;
+	int bNo;
+	String cType;
+	String cOwner;
+	String cAddr;
+	int cPhone;
+	int cFax;
+	String cEmail;
+	int cPic;
+	int cUmony;
+	int cCreditLimit;
+	int cCreditDateMonth;
+	int cCreditDateDay;
+}
