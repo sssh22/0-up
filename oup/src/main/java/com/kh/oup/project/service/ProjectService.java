f5848c7a002111bb65cc16fb83b73d26f93de221
DESKTOP-BTGP2L7\USER <park_sh22@naver.com>
2022-02-24 22:13:54 +0900

    프로젝트 수정중

---
+++ oup/src/main/java/com/kh/oup/project/service/ProjectService.java
@@ -0,0 +1,11 @@
+package com.kh.oup.project.service;
+
+import java.util.List;
+
+import com.kh.oup.project.vo.MemberSearchVo;
+
+public interface ProjectService {
+
+	List<MemberSearchVo> selectList();
+	
+}
---
+++ oup/src/main/java/com/kh/oup/project/vo/MemberSearchVo.java
@@ -0,0 +1,17 @@
+package com.kh.oup.project.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchVo {
+
+	private long employeeNo; //EMPLOYEE
+	private String employeeName; //EMPLOYEE
+	private String teamCode; //EMPLOYEE
+	private char entYN; //EMPLOYEE
+	private String teamName; //TEAM
+	private String departName; //DEPARTMENT
+	
+}
