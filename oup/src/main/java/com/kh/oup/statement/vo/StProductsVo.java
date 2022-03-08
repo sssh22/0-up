f33e23d8dafa773c1922bc509dea3a2652ec9412
DESKTOP-BTGP2L7\USER <park_sh22@naver.com>
2022-03-08 22:12:21 +0900

    0308

---
+++ oup/src/main/java/com/kh/oup/statement/vo/StProductsVo.java
@@ -0,0 +1,15 @@
+package com.kh.oup.statement.vo;
+
+import java.util.Date;
+
+public class StProductsVo {
+
+	int O_LIST_NO;
+	int O_NO;
+	int P_NO;
+	int O_NUM;
+	int O_PRICE;
+	Date O_DELIBERY_DATE;
+
+	
+}
